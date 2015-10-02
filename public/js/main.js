007cbdd1d0ff2306a020c5e827c4bc1bdcfce8b5
hrddd <nuhhunnuhhun@gmail.com>
2015-10-02 14:01:25 +0900

    particle

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,58 @@
-var hello;
+window.requestAnimFrame = (function() {
+  return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
+    return window.setTimeout(callback, 1000 / 60);
+  };
+})();
 
-hello = function() {
-  return console.log("Hello World!");
-};
+$(function() {
+  var Particle, canvas, ctx, density, i, j, loopAnim, particles, ref;
+  canvas = document.querySelector('#slide-container');
+  ctx = canvas.getContext('2d');
+  Particle = (function() {
+    function Particle(scale, color, speed, position) {
+      this.scale = scale;
+      this.color = color;
+      this.speed = speed;
+      this.position = position != null ? position : {
+        x: 100,
+        y: 100
+      };
+    }
 
-hello();
+    Particle.prototype.draw = function(_ctx) {
+      _ctx.beginPath();
+      _ctx.arc(this.position.x, this.position.y, this.scale, 0, 2 * Math.PI, false);
+      _ctx.fillStyle = this.color;
+      return _ctx.fill();
+    };
+
+    return Particle;
+
+  })();
+  density = 100;
+  particles = [];
+  for (i = j = 0, ref = density; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
+    particles[i] = new Particle(6, '#D0A000', Math.random() * (4 - 2) + 2);
+    particles[i].position.x = Math.random() * canvas.width;
+    particles[i].position.y = Math.random() * canvas.height;
+    particles[i].draw(ctx);
+  }
+  loopAnim = function() {
+    var k, len, particle, results;
+    requestAnimFrame(loopAnim);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    results = [];
+    for (k = 0, len = particles.length; k < len; k++) {
+      particle = particles[k];
+      particle.position.y += particle.speed;
+      particle.draw(ctx);
+      if (particle.position.y > canvas.height) {
+        results.push(particle.position.y = -30);
+      } else {
+        results.push(void 0);
+      }
+    }
+    return results;
+  };
+  return loopAnim();
+});
--- resource/coffee/main.coffee
+++ resource/coffee/main.coffee
@@ -1,4 +1,44 @@
-# hello_world
-hello = ->
-    console.log("Hello World!")
-hello()
+window.requestAnimFrame = do ->
+    return  window.requestAnimationFrame   ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        (callback) ->
+            window.setTimeout callback, 1000 / 60
+
+$ ->
+    # property
+    canvas = document.querySelector('#slide-container');
+    ctx = canvas.getContext('2d');
+
+    # particle
+    class Particle
+        constructor: (@scale, @color, @speed,@position = x : 100, y : 100) ->
+        draw: (_ctx) ->
+            _ctx.beginPath()
+            _ctx.arc(@position.x, @position.y, @scale, 0, 2*Math.PI, false)
+            _ctx.fillStyle = @color
+            _ctx.fill()
+
+    # particles    
+    density = 100  # パーティクルの密度
+    particles = [] # パーティクルをまとめる配列
+    
+    for i in [0..density]
+        particles[i] = new Particle(6, '#D0A000', Math.random()*(4-2)+2)
+        particles[i].position.x = Math.random()*canvas.width
+        particles[i].position.y = Math.random()*canvas.height
+        particles[i].draw(ctx)
+
+    loopAnim = ->
+        requestAnimFrame (loopAnim)
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        for particle in particles
+            particle.position.y += particle.speed
+            particle.draw(ctx)
+            if particle.position.y > canvas.height
+                particle.position.y = -30;
+
+    # 実行
+    loopAnim()
